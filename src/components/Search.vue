132e079188d5ee0c2d398dcae1e01f76a6a1daa5
陈珊珊 <chenshanshan@chenshanshandeMac-mini.local>
2019-03-13 20:52:23 +0800

    vue搜索组件

---
+++ src/components/Search.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="search">
+        <div class="search">
+            <form class="search_from">
+                <input type="button" class="search_btn" 
+                @click="searchClick"/>
+                <input type="text" class="search_key" 
+                :placeholder="tip"
+                 v-model="inputTxt"
+                 v-on:keyup.enter="searchClick"/>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"Search",
+    props:{
+        tip:{
+            type:String,
+            default:"请输入关键字"
+        }
+    },
+    data () {
+        return {
+             inputTxt:'',
+        }
+    },
+    methods:{
+        searchClick:function(){
+            this.$emit("doSearch",this.inputTxt)
+        }
+    }
+}
+</script>
+<style scoped>
+
+.search_from{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    width: 224px;
+}
+
+.search_btn{
+    background-color: transparent;
+    border: none;
+    background: url("../assets/sousuo.png") left center no-repeat;
+    margin: 0 0 0 10px; 
+    outline-style: none;
+}
+
+.search_key{
+    background-color: transparent;
+    outline-style: none;
+    margin: 0 10px 0 10px;
+    border: none;
+}
+
+
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Search tip="123" @doSearch="search"></Search>
+    <h1>{{words}}</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Search from '@/components/Search.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Search
+  },
+  data () {
+    return {
+      words:''
+    }
+  },
+  methods: {
+    search:function(param){
+      this.words=param;
+    }
+  }
+}
+</script>
