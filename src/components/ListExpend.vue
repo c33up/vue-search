1c37f5b1707817278afbe25a3c5491f98db8f663
c33up <b236231553@qq.com>
2019-03-13 23:28:11 +0800

    vue

---
+++ src/components/ListExpend.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="list">
+        <div class="group" @click="showItem" >
+            <img :src="show?require('../assets/arr_dowm.png'):
+            require('../assets/arr_right.png')"/>
+            {{items.groups}}</div>
+        <ul v-if="show" class="list">
+            <li
+            class="item" 
+            v-for="(item,index) in items.list" :key="index">
+            {{item}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ListExpend',
+    props: ['items'],
+    data () {
+        return {
+             show:false,
+        }
+    },
+    methods: {
+        showItem:function(){
+            this.show=!this.show;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.group{
+    width: 100%;
+    color: black;
+    text-align: left;
+    border-bottom: 1px solid #333;
+    padding: 10px 0;
+    background-color: pink;
+}
+.list{
+    padding: 0;
+}
+.item{
+    padding: 0;
+    margin: 0;
+    text-align: left;
+    list-style: none;
+    padding: 5px 5px;
+    border-bottom: 1px solid #999;
+    background-color: aliceblue;
+}
+
+</style>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button class="btn" v-on:click="addData">加载数据</button>
+    <div v-for="item in list" :key="item.group" class="groups">
+        <expend-list  :items="item"/>
+    </div>
   </div>
 </template>
+<script>
+import ExpendList from '@/components/ListExpend'
+import { exists } from 'fs';
+export default {
+  name:'About',
+  components:{
+    ExpendList
+  },
+  data () {
+    return {
+      list:[]
+    }
+  },methods: {
+    addData:function(){
+        for(var i=0;i<10;i++){
+          var obj={};
+          obj.groups=i;
+          obj.list=[];
+          for(var x=20;x>1;x--){
+            if(x%i==0){
+              obj.list.push(x);
+            }
+          }
+          this.list.push(obj);
+        }
+        console.table(this.list)
+    }
+  }
+}
+</script>
+
+<style>
+.groups{
+  background-color: #eee;
+  padding-bottom: 10px;
+}
+.btn{
+  padding: 10px 20px;
+  background-color: cadetblue;
+  color: white;
+  margin-bottom: 10px;
+}
+</style>
